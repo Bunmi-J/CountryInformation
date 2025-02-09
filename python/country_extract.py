b50f619f03ab4d202d5c3c2755ea8918a73f31f3
Bunmi-J <obunmi.jegede@gmail.com>
2025-02-09 07:17:11 +0000

    featured:added field region,currencies

--- python/country_extract.py
+++ python/country_extract.py
@@ -20,9 +20,10 @@ def transform(df):
     df["currencies"] = df[currencies].apply(lambda x: ",".join(x.dropna()), axis = 1)
     df = df[["name.common", "name.official", "name.nativeName.eng.common","common_native_name","region","currencies"]]
     print(df[["name.nativeName.eng.common","common_native_name", "region", "currencies"]].head(10))
-# df.to_csv("countrytest.csv", index = False)
-# df = pd.read_csv("countrytest.csv")
-# print(df["name.common"].head())
+    df.to_csv("countrytest.csv", index = False)
+df = pd.read_csv("countrytest.csv")
+print(df["name.common"].head())
+
 
 if __name__ == "__main__":
     data = extract()
